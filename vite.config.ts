522599653d07d3ab609a0b343d5b7416c1968d6a
Talha Rehmani <talharehmani@hotmail.com>
2025-08-24 06:55:23 +0500

    new 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,28 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // 👈 yeh line important hai
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/Loveable_new_web_app/', // 👈 GitHub repo ka naam yahan daalein
-})
+}));
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'), // 👈 yeh line important hai
+//     },
+//   },
+//   base: '/Loveable_new_web_app/', // 👈 GitHub repo ka naam yahan daalein
+// })
